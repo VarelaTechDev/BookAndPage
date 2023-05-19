ef097e980b28bff9ccbf1360ae1f01c951fd34c8
VarelaTechDev <varela.tech.dev@gmail.com>
2023-05-19 16:56:14 -0700

    Got this far in the interview. Now add pages

---
+++ src/main/java/com/arisa/bookandpage/Controller/BookController.java
@@ -0,0 +1,29 @@
+package com.arisa.bookandpage.Controller;
+
+import com.arisa.bookandpage.Entity.Book;
+import com.arisa.bookandpage.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+
+    // Get all books
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public Book addBook(@RequestBody Book book){
+        return bookRepository.save(book);
+    }
+
+}
---
+++ src/main/java/com/arisa/bookandpage/Repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.arisa.bookandpage.Repository;
+
+import com.arisa.bookandpage.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByTitle(String title);
+}
