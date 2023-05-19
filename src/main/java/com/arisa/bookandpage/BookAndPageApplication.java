3ec711637c61bed2b730a6aabb35869aaff0d029
VarelaTechDev <varela.tech.dev@gmail.com>
2023-05-19 16:34:45 -0700

    First

---
+++ src/main/java/com/arisa/bookandpage/BookAndPageApplication.java
@@ -0,0 +1,13 @@
+package com.arisa.bookandpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAndPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAndPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arisa/bookandpage/BookAndPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arisa.bookandpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
